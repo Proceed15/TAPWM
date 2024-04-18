22a2b66548eaa79a036720345736775b1d7f74a0
José Lisboa <104661333+Proceed15@users.noreply.github.com>
2024-04-18 13:20:18 -0300

    Add files via upload

--- Atividade8/script.js
+++ Atividade8/script.js
@@ -4,8 +4,16 @@ function adicionarResposta() {
 const idade = parseInt(document.getElementById("idade").value);
 const sexo = document.querySelector('input[name="sexo"]:checked').value;
 const opiniao = parseInt(document.getElementById("opiniao").value);
+const arrFrobj = Object.keys(respostas);
+const last = arrFrobj.slice(-1);
+++last[1];
+//const last = respostas[respostas.length - 1];
+//const QuerySel = document.querySelectorAll(arrFrobj).length;
+//let numRes = 0;
+//var respondido = 0;
  
-respostas.push({ idade, sexo, opiniao });
+respostas.push({ idade, sexo, opiniao, arrFrobj, last });
+//respondido e numRes --> Método antigo.
  
 calcularEExibirResultados();
 }
@@ -17,11 +25,11 @@ const somaIdades = idades.reduce((acc, idade) => acc + idade, 0);
 const mediaIdade = (somaIdades / respostas.length).toFixed(0);;
 document.getElementById("mediaIdade").textContent = mediaIdade;
  
-const idadeMaisVelha = Math.max(...idades);
-document.getElementById("idadeMaisVelha").textContent = idadeMaisVelha;
+const idadeAvancada = Math.max(...idades);
+document.getElementById("idadeAvancada").textContent = idadeAvancada;
  
-const idadeMaisNova = Math.min(...idades);
-document.getElementById("idadeMaisNova").textContent = idadeMaisNova;
+const idadeJovem = Math.min(...idades);
+document.getElementById("idadeJovem").textContent = idadeJovem;
  
 const qtdPessimo = respostas.filter(resposta => resposta.opiniao === 1).length;
 document.getElementById("qtdPessimo").textContent = qtdPessimo;
@@ -35,4 +43,16 @@ document.getElementById("qtdMulheres").textContent = qtdMulheres;
  
 const qtdHomens = respostas.filter(resposta => resposta.sexo === "masculino").length;
 document.getElementById("qtdHomens").textContent = qtdHomens;
+
+//const numeroRes = respostas.map(resposta => resposta.respondido++);
+
+/*
+for (const key in respostas){
+    length++;
+}
+*/
+
+const qtdPessoas = respostas.map(respostas => respostas.last);
+document.getElementById("qtdPessoas").textContent = qtdPessoas;
+//NumeroRes e length --> métodos antigos.
 }
