b494f46234f2a238e3daf195a1c4cde328851510
José Lisboa Filho <104661333+Proceed15@users.noreply.github.com>
2024-08-15 23:18:09 -0300

    Update Atv11Part1.js

--- Atividade11/Atv11Part1.js
+++ Atividade11/Atv11Part1.js
@@ -1,5 +1,5 @@
 //Declarando as constantes para guardar dados
-const formHasData = {
+const FormHasData = {
     num1: '',
     num2: '',
     num3: '',
@@ -8,21 +8,16 @@ const formHasData = {
 
 function IsData() {
     //Pega os valores do formulário
-    const num1 = document.getElementById('num1').value;
-    const num2 = document.getElementById('num2').value;
-    const num3 = document.getElementById('num3').value;
-    const num4 = document.getElementById('num4').value;
+    const num1 = parseInt(document.getElementById('num1').value);
+    const num2 = parseInt(document.getElementById('num2').value);
+    const num3 = parseInt(document.getElementById('num3').value);
+    const num4 = parseInt(document.getElementById('num4').value);
 
     //Salvando os dados validados
-    formHasData.num1 = num1;
-    formHasData.num2 = num2;
-    formHasData.num3 = num3;
-    formHasData.num4 = num4;
-
-    num1 = parseInt(num1);
-    num2 = parseInt(num2);
-    num3 = parseInt(num3);
-    num4 = parseInt(num4);
+    FormHasData.num1 = num1;
+    FormHasData.num2 = num2;
+    FormHasData.num3 = num3;
+    FormHasData.num4 = num4;
 
     //Mostre uma mensagem de confirmação
     alert('Os dados estão salvos')
@@ -31,45 +26,46 @@ function IsData() {
 
 function IsBigger() {
     // Checando se os dados realmente existem
-    if (formHasData.num1 && formHasData.num2 && formHasData.num3 && formHasData.num4) {
-        alert('Valores informados para os números 1: ' + formHasData.num1 + ', 2: ' + formHasData.num2 + ', 3: ' + formHasData.num3 + ' e 4: ' + formHasData.num4);
+    if (FormHasData.num1 && FormHasData.num2 && FormHasData.num3 && FormHasData.num4) {
+        alert('Valores informados para os números 1: ' + FormHasData.num1 + ', 2: ' + FormHasData.num2 + ', 3: ' + FormHasData.num3 + ' e 4: ' + FormHasData.num4);
         const num1 = parseInt(document.getElementById('num1').value);
         const num2 = parseInt(document.getElementById('num2').value);
         const num3 = parseInt(document.getElementById('num3').value);
         const num4 = parseInt(document.getElementById('num4').value);
         
+        
         if (num1 > num2) {
             if (num1 > num3) {
                 if (num1 > num4) {
-                    alert(formHasData.num1 + ' é o maior número.');
+                    alert(FormHasData.num1 + ' é o maior número.');
                 }
                 else {
-                    alert(formHasData.num4 + ' é o maior número.');
+                    alert(FormHasData.num4 + ' é o maior número.');
                 }
             }
             else {
                 if (num3 > num4){
-                    alert(formHasData.num3 + ' é o maior número.');
+                    alert(FormHasData.num3 + ' é o maior número.');
                 }
                 else {
-                    alert(formHasData.num4 + ' é o maior número.');
+                    alert(FormHasData.num4 + ' é o maior número.');
                 }
             }
         } else {
             if (num2 > num3) {
                 if (num2 > num4) {
-                    alert(formHasData.num2 + ' é o maior número.');
+                    alert(FormHasData.num2 + ' é o maior número.');
                 }
                 else {
-                    alert(formHasData.num4 + ' é o maior número.');
+                    alert(FormHasData.num4 + ' é o maior número.');
                 }
             }
             else {
                 if (num3 > num4) {
-                    alert(formHasData.num3 + ' é o maior número.');
+                    alert(FormHasData.num3 + ' é o maior número.');
                 }
                 else {
-                    alert(formHasData.num4 + ' é o maior número.');
+                    alert(FormHasData.num4 + ' é o maior número.');
                 }
             }
         }
@@ -78,3 +74,40 @@ function IsBigger() {
     }
     //alert("This Works!");
 }
+
+function IsMaximized() {
+    // Checando se os dados realmente existem
+    if (FormHasData.num1 && FormHasData.num2 && FormHasData.num3 && FormHasData.num4) {
+        alert('Valores informados para os números 1: ' + FormHasData.num1 + ', 2: ' + FormHasData.num2 + ', 3: ' + FormHasData.num3 + ' e 4: ' + FormHasData.num4);
+        const ArrayNum = [FormHasData.num1, FormHasData.num2, FormHasData.num3, FormHasData.num4];
+        //Converti os valores em um array e os utilizei com math.max.apply
+        //Para retornar os dados necessários.
+        const MaxNum = Math.max.apply(Math, ArrayNum);
+        alert(MaxNum + ' é o maior número.');
+    } else {
+        alert('Os dados não foram informados corretamente')
+    }
+    //alert("This Works!");
+}
+
+function BeCrescent() {
+    // Checando se os dados realmente existem
+    if (FormHasData.num1 && FormHasData.num2 && FormHasData.num3 && FormHasData.num4) {
+      alert('Valores informados para os números 1: ' + FormHasData.num1 + ', 2: ' + FormHasData.num2 + ', 3: ' + FormHasData.num3 + ' e 4: ' + FormHasData.num4);
+  
+      // Convertendo os valores para números inteiros
+      const num1 = parseInt(FormHasData.num1);
+      const num2 = parseInt(FormHasData.num2);
+      const num3 = parseInt(FormHasData.num3);
+      const num4 = parseInt(FormHasData.num4);
+  
+      // Ordenando os números em ordem crescente com a função Sort(Pesquisei e achei essa função mais fácil aqui)
+      //Criei um array ordenando esse números
+      const ordenados = [num1, num2, num3, num4].sort((a, b) => a - b);
+  
+      // Exibindo a ordem crescente ordenados com join ()
+      alert('A ordem crescente é: ' + ordenados.join(', '));
+    } else {
+      alert('Os dados não foram informados corretamente');
+    }
+  }
