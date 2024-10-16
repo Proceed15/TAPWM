f5d040935fce581ff28c787ced7aaf79b240194b
José Lisboa Filho <104661333+Proceed15@users.noreply.github.com>
2024-10-16 18:13:30 -0300

    Add files via upload

---
+++ Atividade23/SQLQuery4.sql
@@ -0,0 +1,12 @@
+USE [BD]
+GO
+
+INSERT INTO [BD2412016].[professores]
+           ([NOME_PROFESSOR]
+           ,[EMAIL_PROFESSOR])
+     VALUES
+           (<NOME_PROFESSOR, varchar(50),>
+           ,<EMAIL_PROFESSOR, varchar(100),>)
+GO
+
+
